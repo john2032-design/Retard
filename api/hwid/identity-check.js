901cd3018d47babc0bf7a449c0bd3aedc8fdce82
John <faysal.heshmatullah@gmail.com>
2026-05-12 12:31:41 -0300

    Update identity-check.js

--- api/hwid/identity-check.js
+++ api/hwid/identity-check.js
@@ -3,7 +3,7 @@ export const config = { runtime: "nodejs" };
 import crypto from "crypto";
 import { getHwid } from "../../lib/session-store.js";
 
-const INTERNAL_SECRET = process.env.HWID_RESET_SECRET || "";
+const INTERNAL_SECRET = process.env.HWID_RESET_SECRET || "7f3d8a2e4b6c9f1d3a5e7c9b2d4f6a8c0e1d3f5a7b9c1e3d5f7a9b1c3e5d7f9";
 const HWID_SALT = process.env.HWID_SALT || "";
 
 function hashDeviceFingerprint(fingerprintData, hwidSalt) {
